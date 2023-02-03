2183b040cc4b105bb1954a6481ca074f4acdacf1
himanshubanwal <himanshubanwal2001@gmail.com>
2023-02-03 13:41:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-
-import './App.css';
+import "./App.css";
 import React from "react";
-import {
-  HashRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
-import ViewImage from './Screen/Images';
-import Home from './Screen/Home';
-import Detail from './Screen/Detail';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import ViewImage from "./Screen/Images";
+import Home from "./Screen/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/img/:id" element={<ViewImage />} />
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/:id" element={<Detail />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/img/:id" element={<ViewImage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
