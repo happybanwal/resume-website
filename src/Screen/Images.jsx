e2a7a99f4c10266035cf05117be5f64ddd9b9aa1
shubhamwebdesign <hex.media.1800@gmail.com>
2023-02-04 18:02:17 +0530

    view time tracking API integration

--- src/Screen/Images.jsx
+++ src/Screen/Images.jsx
@@ -1,21 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 
-// import { fill } from "@cloudinary/url-gen/actions/resize";
 import { Cloudinary } from "@cloudinary/url-gen";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
 
-// import { thumbnail } from "@cloudinary/url-gen/actions/resize";
-// import { byRadius } from "@cloudinary/url-gen/actions/roundCorners";
-// import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
-// import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
-
 function ViewImage(props) {
   const routeParams = useParams();
-
-  // console.log(routeParams.id);
-
+  const timeNow = useRef(performance.now())
+  console.log({ timeNow })
   const cld = new Cloudinary({
     cloud: {
       cloudName: "dsnvjmelk",
@@ -29,26 +22,42 @@ function ViewImage(props) {
   const myUrl = myImage.toURL();
   // console.log({ myUrl });
 
- const getResumePhoto = async () => {
-   try {
-     const config = {
-       headers: {
-         Accept: "application/json",
-       },
-     };
-     const res = await axios.get(
-       `http://54.167.87.106:8080/api/v1/resume/getResumePhoto/${routeParams.id}`,
-       config
-     );
-     console.log(res);
-   } catch (error) {
-     console.log(error);
-   }
- };
+  const getResumePhoto = async () => {
+    try {
+      const config = {
+        headers: {
+          Accept: "application/json",
+        },
+      };
+      const res = await axios.get(
+        `http://54.167.87.106:8080/api/v1/resume/getResumePhoto/${routeParams.id}`,
+        config
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
- useEffect(() => {
-   getResumePhoto();
- }, []);
+  useEffect(() => {
+    timeNow.current = performance.now()
+    getResumePhoto();
+    // capture component unmount
+    return () => {
+      const timeElapsed = (performance.now() - timeNow.current) / 1000
+      if (timeElapsed > 0.5) {
+        // @todo - call endTracking API body: {time: timeElapsed}
+        console.log(`${timeElapsed} seconds elapsed since you opened the component (:`)
+        axios.post(`http://54.167.87.106:8080/api/v1/resume/analytics/track/${routeParams.id}`, {
+          time: timeElapsed
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        })
+      }
+    }
+  }, []);
 
   return (
     <div>
